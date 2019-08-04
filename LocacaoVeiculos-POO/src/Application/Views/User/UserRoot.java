bc3ba725a181d67fb2ccf1cf32ff4bd2970ed71d
Matheus Milani <m_milani@hotmail.com>
2019-08-04 14:24:25 -0300

    User rentable vehicle view and action

--- LocacaoVeiculos-POO/src/Application/Views/User/UserRoot.java
+++ LocacaoVeiculos-POO/src/Application/Views/User/UserRoot.java
@@ -1,7 +1,40 @@
 package Application.Views.User;
 
-public class UserRoot {
+import Application.VehicleController;
+import Application.ApplicationController;
+import Application.SessionController;
+import Application.Views.BaseView;
+import Models.User;
+
+public class UserRoot extends BaseView {
+	static User user = SessionController.authenticatedUser;
+	
 	public static void render() {
-		System.out.println("UserRoot");
+		System.out.println("Bem Vindo, " + user.getName() + "\n");
+		
+		System.out.println("1 - Lista de veiculos para locação");
+
+		System.out.println("\nX - Sair");
+		
+		userRoutes();
+	}
+	
+	private static void userRoutes() {
+		String option = read();
+		
+		while(true)	{			
+			switch (option) {
+			case "1":
+				VehicleController.rentIndex();
+				return;
+			case "X":
+				ApplicationController.start();
+				return;
+			default:
+				System.out.println("Você deve escolher uma opção válida");
+				option = read();
+				break;
+			}
+		}
 	}
 }
--- LocacaoVeiculos-POO/src/Application/Views/Vehicle/VehicleShow.java
+++ LocacaoVeiculos-POO/src/Application/Views/Vehicle/VehicleShow.java
@@ -1,29 +1,52 @@
 package Application.Views.Vehicle;
 
+import Application.SessionController;
+import Application.VehicleController;
 import Application.Views.BaseView;
+import Models.Employee;
+import Models.User;
 import Models.Vehicle;
 
 public class VehicleShow extends BaseView {
-
+	private static User authUser = SessionController.authenticatedUser;
+	private static Employee employeeUser = SessionController.authenticatedEmployee;
+	private static Vehicle viewdVehicle;
 	public static void render(Vehicle vehicle) {
-		System.out.println("\nDetalhes do veículo\n");
-		System.out.println("ID: " + vehicle.getId());
-		System.out.println("Modelo: " + vehicle.getId());
-		System.out.println("Fabricante: " + vehicle.getId());
-		System.out.println("Categoria: " + vehicle.getId());
-		System.out.println("Status: " + vehicle.getId());
-		System.out.println("Tarifa diária: " + vehicle.getId());
+		viewdVehicle = vehicle;
 		
-		System.out.println("Locações: TODO");
+		System.out.println("\nDetalhes do veículo\n");
+		
+		System.out.println("ID: " + vehicle.getId());
+		System.out.println("Modelo: " + vehicle.getModel());
+		System.out.println("Fabricante: " + vehicle.getManufacturer());
+		System.out.println("Categoria: " + vehicle.getCategory());
+		System.out.println("Status: " + vehicle.getStatus());
+		System.out.println("Tarifa diária: " + vehicle.getDailyFee());
+
+		if(employeeUser == null) {
+			System.out.println("\nPara reservar este carro para locação, digite 'S'");			
+		}
 		
 		System.out.println("\nDigite X para sair");
 		
-		String option = read();
-		while(!option.equals("X")) {
-			option = read();
-		}
-		
-		VehicleRoot.render();
+		getUserOption();
 	}
-
+	
+	private static void getUserOption() {
+		String option = read();
+		
+		switch (option) {
+		case "S":
+			System.out.println("\nDigite por quantos dias deseja alugar este veiculo:");
+			String days = read();
+			
+			VehicleController.rent(authUser, viewdVehicle.getId(), days);
+			break;
+		case "X":
+			VehicleController.start();
+			break;
+		default:
+			break;
+		}
+	}
 }
